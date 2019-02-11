e587d99a0dabee8eb951c5965989d97b12c8e4a0
sushil <sushil@nvestbank.com>
2019-02-11 11:35:36 +0530

    code update

--- src/component/history/History.js
+++ src/component/history/History.js
@@ -5,13 +5,13 @@ import { UncontrolledCollapse, CardBody, Card } from 'reactstrap';
 class History extends Component {
     state = {
         tokenDetail: [
-            { id: "one1",triggerId:"#one1", message: "Sent Tokens", status: "CONFIRMED", amount: 2324, amountUSD: 655, amountDate:"1/22/2019" },
-            { id: "two1",triggerId:"#two1", message: "Sent Tokens", status: "PENDING", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
-            { id: "two2",triggerId:"#two2", message: "Sent Tokens", status: "PENDING", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
-            { id: "two3",triggerId:"#two3", message: "Sent Tokens", status: "CONFIRMED", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
-            { id: "two4",triggerId:"#two4", message: "Sent Tokens", status: "PENDING", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
-            { id: "two5",triggerId:"#two5", message: "Sent Tokens", status: "CONFIRMED", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
-            { id: "two6",triggerId:"#two6", message: "Sent Tokens", status: "PENDING", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
+            { id: "one1", message: "Sent Tokens", status: "CONFIRMED", amount: 2324, amountUSD: 655, amountDate:"1/22/2019" },
+            { id: "two1", message: "Sent Tokens", status: "PENDING", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
+            { id: "two2", message: "Sent Tokens", status: "PENDING", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
+            { id: "two3", message: "Sent Tokens", status: "CONFIRMED", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
+            { id: "two4", message: "Sent Tokens", status: "PENDING", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
+            { id: "two5", message: "Sent Tokens", status: "CONFIRMED", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
+            { id: "two6", message: "Sent Tokens", status: "PENDING", amount: 234, amountUSD: 55, amountDate:"1/24/2019" },
         ]
     }
     toggleCollapse = collapseID => () =>
@@ -39,7 +39,7 @@ class History extends Component {
                             <span>{tokenDetail.amountUSD} USD</span>
                         </div>
                     </div>
-                    <UncontrolledCollapse toggler={tokenDetail.triggerId}>
+                    <UncontrolledCollapse toggler={tokenDetail.id}>
                         <Card>
                             <CardBody className="cstmbdycrd">
                             <div>
